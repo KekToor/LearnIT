5006c86bbe62f4e2c199914c520a4f958559bd5b
KekToor <p4fczgd@gmail.com>
2022-12-26 12:09:53 +0100

    Frontend - Paginace

--- front/pages/codes.js
+++ front/pages/codes.js
@@ -1,8 +1,13 @@
 import Layout from "../components/Layout";
 import { fetcher } from "../lib/api";
 import Codes from "../components/Codes";
+import useSWR from 'swr'
+import {useState} from "react";
 
 const CodesList = ({ codes }) => {
+    const [pageIndex, setPageIndex] = useState(1);
+    const { data } = useSWR(`${process.env.NEXT_PUBLIC_STRAPI_URL}/codes?pagination[page]=${pageIndex}&pagination[pageSize]=2`, fetcher, {fallbackData: codes});
+    console.log(data.meta.pagination.pageCount);
     return (
         <Layout>
             <h1 className="flex justify-center text-4xl font-extrabold mb-3">
@@ -12,7 +17,43 @@ const CodesList = ({ codes }) => {
 
             </h1>
             <div className="text-left">
-                <Codes codes={codes}/>
+                <Codes codes={data}/>
+                <div className='space-x-2 space-y-2'>
+                    <button className={`md:p-2 rounded py-2 text-black dark:text-white p-2 ${pageIndex === 1 ? 'bg-gray-500' : 'bg-blue-400'}`}
+                            disabled={pageIndex === 1}
+                            onClick={() => setPageIndex(pageIndex - 1)}
+                    >
+                        {' '}
+                        Předchozí
+                    </button>
+                    <button className={`md:p-2 rounded py-2 text-black dark:text-white p-2
+                                        ${pageIndex === (data && data.meta.pagination.pageCount)
+                                        ? 'bg-gray-500' : 'bg-blue-400'}`}
+                            disabled={pageIndex === (data && data.meta.pagination.pageCount)}
+                            onClick={() => setPageIndex(pageIndex + 1)}
+                    >
+                        {' '}
+                        Další
+                    </button>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div>
+                            {`Zobrazuje se záznam ${(pageIndex - 1) * (data && data.meta.pagination.pageSize) + 1}
+                            ${ ((pageIndex - 1) * (data && data.meta.pagination.pageSize) + 1) === (data && data.meta.pagination.total) ?
+                                '' : `- ${((pageIndex * (data && data.meta.pagination.pageSize)) > (data && data.meta.pagination.total)) ?
+                                (data && data.meta.pagination.total)
+                                : pageIndex * (data && data.meta.pagination.pageSize)}`
+                            
+                            } ze ${data && data.meta.pagination.total} záznamů`}
+                        </div>
+                        <div className='text-right'>
+                            {`Stránka ${pageIndex} ze ${
+                            data && data.meta.pagination.pageCount
+                        }`}
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </Layout>
     );
@@ -20,8 +61,8 @@ const CodesList = ({ codes }) => {
 
 export default CodesList;
 
-export async function getServerSideProps() {
-    const codesRes = await fetcher( `${process.env.NEXT_PUBLIC_STRAPI_URL}/codes`);
+export async function getStaticProps() {
+    const codesRes = await fetcher( `${process.env.NEXT_PUBLIC_STRAPI_URL}/codes?pagination[page]=1&pagination[pageSize]=2`);
     console.log(codesRes);
     return{
         props: {
